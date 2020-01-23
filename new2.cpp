4c3f1ba087d2bbde8113abdd40a6605fb3e36800
Sourabh Sharma <40396976+sourabhpy@users.noreply.github.com>
2020-01-23 23:52:08 +0530

    Update new2.cpp

--- new2.cpp
+++ new2.cpp
@@ -1,45 +1,52 @@
-// Write a program to delete an element from an array. Even if it exists multiple times, remove it completely.
+// A program to delete an element from an array. Even if it exists multiple times, remove it completely.
+// Author : sourabh.py
+// Build Date : Jan 23, 2020 | 11:53 PM
 
 #include<iostream>
 using namespace std;
 
 int main(){
-    int sizeofarray, i, j, number, position, countofelements=0;
-    cout << "Enter the size of the array : " << endl;
-    cin >> sizeofarray;
-    int a[sizeofarray];
-    cout << "Now enter the array elements : " << endl;
-    for (i=0; i<sizeofarray; i++){
-        cin >> a[i];
-    }
-    cout << "The entered array is : " << endl;
-    for (i=0; i<sizeofarray; i++){
-        cout << a[i] << " ";
-    }
-    cout << endl;
-    cout << "Now enter the number you want to completely delete from the array." << endl;
-    cin >> number;
-    for (i=0; i<sizeofarray; i++){
-        if(number == a[i]){
-            countofelements++;
-            cout << "The number exists at position " << i << "." << endl;
-        }
-    }
-    for (i=0; i<sizeofarray; i++){
-        if(number == a[i]){
-            position = i;
-            for (i=position; i<sizeofarray; i++){
-                a[i] = a[i+1];
-            }
-        }
-    }
-    sizeofarray = sizeofarray - countofelements;
-    cout << "The total occurrences of the entered element were " << countofelements << "." << endl;
-    cout << "The element has been deleted from the array. The new array is : " << endl;
-    for (i=0; i<sizeofarray; i++){
-        cout << a[i] << " ";
-    }
-    cout << endl;
-    cout << "Done." << endl;
-    return 0;
+	int size, i, j, element, count=0, flag=0;
+	cout << "Enter the size of the array : " << endl;
+	cin >> size;
+	int a[size];
+	cout << "Now enter the array elements : " << endl;
+	for (i=0; i<size; i++){
+		cin >> a[i];
+	}
+	cout << "The entered array is : " << endl;
+	for (i=0; i<size; i++){
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	cout << "Now enter the element you want to delete from the array : " << endl;
+	cin >> element;
+	for (i=0; i<size; i++){
+		if (element == a[i]){
+			flag=1;
+			count++;
+			cout << "The element is at position " << i << "." << endl;
+		}
+	}
+	for (i=0; i<size; i++){
+		if (element == a[i]){
+			for (j=i; j<size; j++){
+				a[j] = a[j+1];
+			}
+		}
+	}
+	size = size - count;
+	if (flag == 1)
+		cout << "The total occurences of this element were " << count << "." << endl;
+	else if (flag == 0)
+		cout << "There was only 1 occurence of this element." << endl;
+	else
+	    cout << "Unknown error occured. Please check your code and try again later." << endl;
+	cout << "The array has been updated." << endl << "The new array is : " << endl;
+	for (i=0; i<size; i++){
+		cout << a[i] << " ";
+	}
+	cout << endl;
+	cout << "Process exited with a return value 0. Press any key to continue . . . " << endl;
+	return 0;
 }
